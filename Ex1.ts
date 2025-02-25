5580e052f2a5a9a7d4925422bc2eb3ba01f4a6b7
Renu-13 <129666034+Renu-13@users.noreply.github.com>
2025-02-25 10:25:24 +0530

    Add files via upload

---
+++ Ex1.ts
@@ -0,0 +1,13 @@
+//An array of numbers
+let numbers:number[] = [10,20,30];
+
+//An array of Strings
+//let fruits: string[] = ["Apple","Banana"];
+
+//An array of booleans
+//let flag: boolean[] = [true,false];
+
+//An mixedArray
+//let array: (number|string)[] = [1,"apple",2,"banana"];
+
+console.log("Numbers:",numbers);
